1f9e74128b7eb49992a295f77dd172f3763b66f8
Maxim Frolov <consumer2@mail.ru>
2015-04-07 12:41:15 +0500

    added the MVC pattern and the gitignore file

---
+++ views/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<h1>Новости</h1>
+<?php foreach ($news as $article) : ?>
+<article>
+    <h3>
+        <a href="/article.php?id=<?php echo $article['id']; ?>">
+            <?php echo $article['title']; ?>
+        </a>
+    </h3>
+    <div><?php echo $article['text']; ?></div>
+</article>
+<?php endforeach; ?>
+</body>
+</html>
