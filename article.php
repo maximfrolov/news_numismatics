99689b9497a1a5d64c6a35b36779810dca07fcfd
Maxim Frolov <consumer2@mail.ru>
2015-04-08 00:43:09 +0500

    restructured folders and files

--- article.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . '/models/news.php';
-
-$article = findOneById($_GET['id']);
-
-include __DIR__ . '/views/article.php';
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . '/models/news.php';
-
-$news = findAllNews();
-
-include __DIR__ . '/views/index.php';
