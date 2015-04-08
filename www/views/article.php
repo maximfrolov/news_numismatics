49abb61b553457e5c95cd68f56887a780cd4d0a2
Maxim Frolov <consumer2@mail.ru>
2015-04-08 12:33:35 +0500

    changed views/article.php

--- www/views/article.php
+++ www/views/article.php
@@ -5,12 +5,21 @@
     <title></title>
 </head>
 <body>
-<h1>Новость</h1>
+<h1>Новость дня!</h1>
 <article>
     <h3>
         <?php echo $article['title']; ?>
     </h3>
     <div><?php echo $article['text']; ?></div>
+    <p>
+        Дата публикации: <?php echo $article['date']; ?>
+    </p>
+    <p>
+        По материалам:
+        <a href="<?php echo $article['source']; ?>">
+            <?php echo $article['source']; ?>
+        </a>
+    </p>
 </article>
 </body>
 </html>
