173d308366a525a9f0bbe901acbef323bcc0dffb
Maxim Frolov <consumer2@mail.ru>
2015-04-09 00:43:49 +0500

    changed add.php and views/add.php

--- www/add.php
+++ www/add.php
@@ -1,10 +1,9 @@
 <?php
 
-include __DIR__ . '/views/add.php';
+include_once __DIR__ . '/views/add.php';
+
+require_once __DIR__ . '/models/news.php';
 
 if (isset($_POST['add'])) {
-    require __DIR__ . '/models/news.php';
     addOneNews();
-    exit;
 }
-
--- www/views/add.php
+++ www/views/add.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 
-<form method="post" action="">
+<form method="post" action="/add.php">
     Название новости:
     <br>
     <input type="text" name="title">
