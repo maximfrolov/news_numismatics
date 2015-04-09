7244dd0203a88e185f50b62258396e3f35997fbd
Maxim Frolov <consumer2@mail.ru>
2015-04-09 22:02:52 +0500

    chenged /add.php

--- www/add.php
+++ www/add.php
@@ -1,9 +1,10 @@
 <?php
 
-include_once __DIR__ . '/views/add.php';
-
-require_once __DIR__ . '/models/news.php';
+require __DIR__ . '/models/news.php';
 
 if (isset($_POST['add'])) {
     addOneNews();
 }
+
+include __DIR__ . '/views/add.php';
+
