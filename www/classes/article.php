d88d470f2857047ec89957bbbe6e863a1f9f7238
Maxim Frolov <consumer2@mail.ru>
2015-04-13 21:32:53 +0500

    created a new file article.php for the abstract class article

---
+++ www/classes/article.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class Article {
+
+    public $title;
+    public $text;
+    public $date;
+}
+
+class NewsArticle extends Article {
+
+    public $author;
+}
+
+class RepostArticle extends Article {
+
+    public $source;
+
+}
