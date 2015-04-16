6e7025da81ac2e8bebbe669dbddc5c6b5c83c8d6
Maxim Frolov <consumer2@mail.ru>
2015-04-16 11:32:55 +0500

    удалил старую вью index.php, изменил вью all.php

--- www/views/news/all.php
+++ www/views/news/all.php
@@ -5,11 +5,26 @@
     <title></title>
 </head>
 <body>
+<h1>Новости из мира нумизматики</h1>
+<a href="/form.php">
+    Добавить новость
+</a>
 
 <?php foreach ($items as $item) : ?>
     <article>
         <h1><?php echo $item->title; ?></h1>
         <div><?php echo $item->text; ?></div>
+        <div>
+            <p>
+                <?php echo $item->date; ?>
+            </p>
+            <p>
+                <?php echo $item->author; ?>
+            </p>
+            <p>
+                <?php echo $item->source; ?>
+            </p>
+        </div>
     </article>
 <?php endforeach ?>
 
